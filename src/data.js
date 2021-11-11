6fc7f3a223ec561f8580c5672696b161efe56258
FranGLeone <frangleone@gmail.com>
2021-11-11 20:41:22 -0300

    corrigiendo desafio botones

--- src/data.js
+++ src/data.js
@@ -1,23 +1,27 @@
 export const data = [
     {
-      nombre: "Top coat Matte",
-      precio: 7, //este funciona como cantidad. ya que al agregar la prop cantidad no me funciona inexplicablemente
-      cantidad: 8,
-      thumbnail: "https://http2.mlstatic.com/D_NQ_NP_750508-MLA41598057193_042020-O.jpg",
+        id: 1,
+        nombre: "Top coat Matte",
+        precio: 7, //este funciona como cantidad. ya que al agregar la prop cantidad no me funciona inexplicablemente
+        cantidad: 8,
+        thumbnail: "https://http2.mlstatic.com/D_NQ_NP_750508-MLA41598057193_042020-O.jpg",
     },
     {
+        id: 2,
         nombre: "Blanco French",
         precio: 6,
         cantidad: 6,
         thumbnail: "https://http2.mlstatic.com/D_NQ_NP_750591-MLA31637613465_072019-O.jpg",
     },
     {
+        id: 3,
         nombre: "Deep Blue Sky",
         precio: 4,
         cantidad: 4,
         thumbnail: "https://http2.mlstatic.com/D_NQ_NP_866024-MLA45659817153_042021-V.jpg",
     },
     {
+        id: 4,
         nombre: "White Milky Way",
         precio: 0,
         cantidad: 5,
